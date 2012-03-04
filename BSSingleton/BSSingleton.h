21d89b5a35d1d4cd64d96d5d3daeaf482e9a5cbd
beepscore <support@beepscore.com>
2012-03-04 09:57:27 -0800

    Add class BSSingleton.

---
+++ BSSingleton/BSSingleton.h
@@ -0,0 +1,13 @@
+//
+//  BSSingleton.h
+//  BSSingleton
+//
+//  Created by Steve Baker on 3/4/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSSingleton : NSObject
+
+@end
---
+++ BSSingleton/BSSingleton.m
@@ -0,0 +1,13 @@
+//
+//  BSSingleton.m
+//  BSSingleton
+//
+//  Created by Steve Baker on 3/4/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "BSSingleton.h"
+
+@implementation BSSingleton
+
+@end
